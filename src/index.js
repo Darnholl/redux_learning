5588887918232904ccb2aa4b6ce2779c37a46a29
Alexey Blinov <Rasiel1989@gmail.com>
2023-06-08 09:50:37 +0300

    first HW

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import * as actions from "./store/actions";
+import { initiateStore } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = initiateStore();
+
+const App = () => {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
+
+  const completeTask = (taskId) => {
+    store.dispatch(actions.taskCompleted(taskId));
+  };
+  const changeTitle = (taskId) => {
+    store.dispatch(actions.titleChanged(taskId));
+  };
+  const deleteTitle = (taskId) => {
+    store.dispatch(actions.titleDeleted(taskId));
+  };
+
+  return (
+    <>
+      <h1>app</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p>{`Completed: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Complete</button>
+            <button onClick={() => changeTitle(el.id)}>Change title</button>
+            <button onClick={() => deleteTitle(el.id)}>Delete</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
